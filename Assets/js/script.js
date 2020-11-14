e6c80a578586f5518817e91f5b43ce564dc56a3d
Russell Jewett <rfjewett3@gmail.com>
2020-11-14 00:12:29 -0500

    Makes changes to logic. Array needs addressing.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,144 +1,155 @@
-
 var startButton = document.getElementById('start-btn');
 var description = document.getElementById('description');
 var questionContainerEl = document.getElementById('question-container');
 var questionEl = document.getElementById('question');
-var answerBtns = document.getElementById('answer-buttons');
+var answerButtonsEl = document.getElementById('answer-buttons');
 var currentQuestionIndex;
 var sec = 60;
 var score = 0;
 
-
-// Add an event listnener to wait for the user to click 'start'
+// Add an event listener to wait for the user to click 'start'
 startButton.addEventListener('click', startGame);
 
-// Shuffle the questions prior to the quiz starting using a switching algorithm.
-function shuffleArray(array) {
-    for (var i; i < array.length; i++) {
-        var rand = Math.floor(Math.random() * array.length);
-        var temp = array[i];
-        array[i] = array[rand];
-        array[rand] = temp;
+//Shuffle the questions prior to the quiz starting using a switching algorithm.
+function shuffleArray(passedArray) {
+    console.log("this is my passedArray: " + passedArray[1].questionText);
+    for (var i; i < passedArray.length; i++) {
+        var rand = Math.floor(Math.random() * passedArray.length);
+        var temp = passedArray[i];
+        passedArray[i] = passedArray[rand];
+        passedArray[rand] = temp;
+        console.log("testing" + passedArray[i].questionText);
     }
-    return array;
+    console.log("this is my passedArray: " + passedArray[1].questionText);
+    return passedArray;
 }
 
-// Create Timer
+//Create Timer
 function timer() {
-    var timer = setInterval(function() {
+    var timer = setInterval(function () {
         document.getElementById('timer').textContent = 'Time: ' + sec;
         if (sec <= 0) {
             clearInterval(timer);
             // endQuiz;
-        } 
+        }
         sec--;
     }, 1000);
 }
 
-
+// Create a function that starts the game.
 function startGame() {
     timer();
     startButton.classList.add('hide');
-    description.classList.add('hide');
-    shuffleArray(array);
+    description.classList.add('hide')
+    shuffledQuestions = shuffleArray(questionArray);
+    console.log("this is my questionArray: " + shuffleArray[1].questionText);
     currentQuestionIndex = 0;
     questionContainerEl.classList.remove('hide');
-    initializeQuestions();
+    initializeQuestion();
 }
 
-// Show questions until questions run out.
-// Initialize event listeners for each answer choice.
-function initializeQuestions() {
-    showQuestion(array[currentQuestionIndex++])
-    while (currentQuestionIndex < array.length) {
-        showQuestion(array[currentQuestionIndex++]);
-        //checker();
+function initializeQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    while (currentQuestionIndex < questionArray.length) {
+        console.log(currentQuestionIndex);
+        showQuestion(questionArray[++currentQuestionIndex]);
     }
-    // endQuiz;
+    // endQuiz
 }
 
 function showQuestion(question) {
-    //add the question text to the screen
-    questionEl.textContent = question.question;
-    //loop through for each answer choice
-    for(var i = 0; i < question.answers.length; i++){
-        console.log("Hit");
-        //create a new button
-        var answerButton = document.createElement("BUTTON");
-        answerButton.textContent = question.answers[i].text;
-        answerButton.setAttribute("CorrectAnswer", question.answers[i].correct);
-        answerButton.classList.add('btn');
-        // TODO:answerButton.addEventListener('click', checker(event));
-        
-        //add button to answerButton Array
-        answerBtns.appendChild(answerButton);
+    // Modify the "question" element in the html to show corresponding question in the questionArray.
+    questionEl.textContent = question.questionText;
+    // Populate each button with corresponding text and add an event listener for when the user selects an answer choice
+    var answerButton = document.getElementById('answer-buttons').children[0];
+    answerButton.textContent = question.answers[0].text;
+    answerButton.addEventListener("click", function () {
+        selectAnswer(question.answers[0].correct);
+    });
+    var answerButton = document.getElementById('answer-buttons').children[1];
+    answerButton.textContent = question.answers[1].text;
+    answerButton.addEventListener("click", function () {
+        selectAnswer(question.answers[1].correct);
+    });
+    var answerButton = document.getElementById('answer-buttons').children[2];
+    answerButton.textContent = question.answers[2].text;
+    answerButton.addEventListener("click", function () {
+        selectAnswer(question.answers[2].correct);
+    });
+    var answerButton = document.getElementById('answer-buttons').children[3];
+    answerButton.textContent = question.answers[3].text;
+    answerButton.addEventListener("click", function () {
+        selectAnswer(question.answers[3].correct);
+    });
+}
+
+function selectAnswer(isCorrect) {
+    console.log(isCorrect);
+    if (isCorrect == true) {
+        score += 10;
+    } else {
+        sec -= 10;
     }
+    // currentQuestionIndex++;
 }
 
 // Create the questions and Answers in an array to reference in showQuestion.
-var array = [
+var questionArray = [
     {
-        question: "What is the format used for storing and transporting data?",
+        questionText: "What is the format used for storing and transporting data?",
         answers: [
-            {text: 'JASON', correct: false},
-            {text: 'JSON', correct: true},
-            {text: 'jQuery', correct: false},
-            {text: 'JavaScript', correct: false}
+            { text: 'JASON', correct: false },
+            { text: 'JSON', correct: true },
+            { text: 'jQuery', correct: false },
+            { text: 'JavaScript', correct: false }
         ],
-        question: "What does 'css' stand for?",
+    }, {
+        questionText: "What does 'css' stand for?",
         answers: [
-            {text: 'cascading style sheets', correct: true},
-            {text: 'create something special', correct: false},
-            {text: 'cool style sheets', correct: false},
-            {text: 'call syntax sheets', correct: false}
+            { text: 'cascading style sheets', correct: true },
+            { text: 'create something special', correct: false },
+            { text: 'cool style sheets', correct: false },
+            { text: 'call syntax sheets', correct: false }
         ],
-        question: "What is used to enclose an array?",
+    }, {
+        questionText: "What is used to enclose an array?",
         answers: [
-            {text: 'Nothing', correct: false},
-            {text: 'Quotation Marks', correct: false},
-            {text: 'Curly Brackets', correct: false},
-            {text: 'Square Brackets', correct: true}
+            { text: 'Nothing', correct: false },
+            { text: 'Quotation Marks', correct: false },
+            { text: 'Curly Brackets', correct: false },
+            { text: 'Square Brackets', correct: true }
         ],
-        question: "How would you check if A is equal to 2?",
+    }, {
+        questionText: "How would you check if A is equal to 2?",
         answers: [
-            {text: 'A ?= 2', correct: false},
-            {text: 'A != 2', correct: false},
-            {text: 'A == 2', correct: true},
-            {text: 'Is A 2?', correct: false}
+            { text: 'A ?= 2', correct: false },
+            { text: 'A != 2', correct: false },
+            { text: 'A == 2', correct: true },
+            { text: 'Is A 2?', correct: false }
         ],
-        question: "How do you comment something in Javascript?",
+    }, {
+        questionText: "How do you comment something in Javascript?",
         answers: [
-            {text: '<!--', correct: false},
-            {text: '#', correct: false},
-            {text: '//', correct: true},
-            {text: 'comment:', correct: false}
+            { text: '<!--', correct: false },
+            { text: '#', correct: false },
+            { text: '//', correct: true },
+            { text: 'comment:', correct: false }
         ],
-        question: "What is used to declare a variable in JavaScript?",
+    }, {
+        questionText: "What is used to declare a variable in JavaScript?",
         answers: [
-            {text: 'var =', correct: true},
-            {text: 'var: ', correct: false},
-            {text: 'variable: ', correct: false},
-            {text: 'var()', correct: false}
+            { text: 'var =', correct: true },
+            { text: 'var: ', correct: false },
+            { text: 'variable: ', correct: false },
+            { text: 'var()', correct: false }
         ],
-        question: "What is used to see if a value AND data type are the same?",
+    }, {
+        questionText: "What is used to see if a value AND data type are the same?",
         answers: [
-            {text: '&&', correct: false},
-            {text: '/=', correct: false},
-            {text: '==', correct: false},
-            {text: '===', correct: true}
+            { text: '&&', correct: false },
+            { text: '/=', correct: false },
+            { text: '==', correct: false },
+            { text: '===', correct: true }
         ]
     }
 ]
-
-// Check to see if the user click the right answer to the question.
-function checker() {
-    //if (array.answers[correct] == true) {
-        score += 10;
-   // } else {
-        sec -= 10;
- //   }
-}
-
-function endQuiz() {
-    questionContainerEl.classList.add('hide');
-}
