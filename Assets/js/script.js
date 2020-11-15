cfce2654d0ea3015948672c45a8b4b31219f8659
Russell Jewett <rfjewett3@gmail.com>
2020-11-14 23:38:13 -0500

    Fixes bugs. Quiz is working.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,30 @@
+var scores = document.getElementById('score');
+var clock = document.getElementById('timer');
+var leaderboard = document.getElementById('leaders');
 var startButton = document.getElementById('start-btn');
 var description = document.getElementById('description');
 var questionContainerEl = document.getElementById('question-container');
 var questionEl = document.getElementById('question');
 var answerButtonsEl = document.getElementById('answer-buttons');
-var userInitials = document.getElementById('user-initials');
-var resetButton = document.getElementById('reset')
+var submit = document.getElementById('submit');
+var userInitials = document.getElementById('initials');
+var currentQuestionIndex;
 var sec = 60;
 var score = 0;
+
+// Add an event listener to wait for the user to click 'start'
+startButton.addEventListener('click', startGame);
+
+//Shuffle the questions prior to the quiz starting using a switching algorithm.
+function shuffleArray(passedArray) {
+    for (var i = 0; i < passedArray.length; i++) {
+        var rand = Math.floor(Math.random() * passedArray.length);
+        var temp = passedArray[i];
+        passedArray[i] = passedArray[rand];
+        passedArray[rand] = temp;
+    }
+    return passedArray;
+}
 
 //Create Timer
 function timer() {
@@ -20,75 +38,98 @@ function timer() {
     }, 1000);
 }
 
-// Add an event listener to wait for the user to click 'start', then begin the quiz.
-startButton.addEventListener('click', function () {
+var shuffled = [];
+
+// Create a function that starts the game.
+function startGame() {
     timer();
-    questionContainerEl.classList.remove('hide');
     startButton.classList.add('hide');
-    description.classList.add('hide');
-    createQuestion();
-});
-
-// Use a for loop to populate the HTML with question text at index [i] and answers attached to that question.
-// Also set the data type for each question for validation later.
-function createQuestion() {
-    var questionNumber = 0;
-    if (questionNumber === questionArray.length) {
-        endQuiz;
-    } else {
-        questionContainerEl.append(`<p>${questionArray[questionNumber].questionText}`);
-        for (i = 0; i < questionArray[questionNumber].answers.length; i++) {
-            answerButtonsEl.append(`<button class="answer${i} answerBtn btn" dataType = "${questionArray[questionNumber].answers[i].correct}">${questionArray[questionNumber].answers[i].text}</button>`);
-            questionNumber++;
-        }
-    }
+    description.classList.add('hide')
+    shuffled = shuffleArray(questionArray);
+    currentQuestionIndex = 0;
+    questionContainerEl.classList.remove('hide');
+    initializeQuestion(shuffled);
 }
 
-// Check whether or not the user selected the correct answer and apply consequences accordingly.
-function handleAnswer() {
-    if (answerButtonsEl.dataType === true) {
-        score += 10;
-        alert('Correct! You get 10 points!');
-    } else {
-        sec -= 10;
-        alert('Wrong! you lose 10 seconds!');
-    }
+function initializeQuestion(shuffledQuestions) {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
-// After each answer selection has been checked, make sure to clear the answers so they can be re-filled in the next iteration of the for-loop.
 function clearAnswers() {
     answerButtonsEl.innerHTML = "";
 }
 
-// Once the user clicks their selection, the selection is checked for correctness, the answerButton HTML text is cleared, and a new question is created.
-answerButtonsEl.addEventListener('click', function () {
-    handleAnswer();
+function showQuestion(currentQuestionObject) {
+    // Modify the "question" element in the html to show corresponding question in the questionArray.
+    questionEl.textContent = currentQuestionObject.questionText;
+    // Clear previous answers from buttons to make room for new ones.
     clearAnswers();
-    createQuestion();
-})
-
-// When the game ends, the user has the option to log their initials to keep their score.
-// addInitials.addEventListener('click', function() {
-//     userInitials.textInput.value();
-//     var jsonInitials = JSON.stringify(userInitials);
-//     var jsonScore = JSON.strungify(score);
-//     localStorage.setItem({user: jsonInitials, score: jsonScore});
-// })
-
-// If a user wishes to clear all of the high-scores listed, this fuction will do just that.
-// resetButton.addEventListener('click', function() {
-//     localStorage.clear();
-// })
-
-// Once a user has reached the final question, they will be shown the score display page.
-function endQuiz() {
-    alert("The quiz is over");
+    // Populate each button with corresponding text and add an event listener for when the user selects an answer choice
+    var answerbtn = document.createElement('answerbtn');
+    answerbtn.textContent = currentQuestionObject.answers[0].text;
+    answerbtn.classList.add('btn', 'answer');
+    document.getElementById('answer-buttons').appendChild(answerbtn);
+    answerbtn.addEventListener('click', function () {
+        selectAnswer(currentQuestionObject.answers[0].correct);
+    })
+    var answerbtn = document.createElement('answerbtn');
+    answerbtn.textContent = currentQuestionObject.answers[1].text;
+    answerbtn.classList.add('btn', 'answer');
+    document.getElementById('answer-buttons').appendChild(answerbtn);
+    answerbtn.addEventListener('click', function () {
+        selectAnswer(currentQuestionObject.answers[1].correct);
+    })
+    var answerbtn = document.createElement('answerbtn');
+    answerbtn.textContent = currentQuestionObject.answers[2].text;
+    answerbtn.classList.add('btn', 'answer');
+    document.getElementById('answer-buttons').appendChild(answerbtn);
+    answerbtn.addEventListener('click', function () {
+        selectAnswer(currentQuestionObject.answers[2].correct);
+    })
+    var answerbtn = document.createElement('answerbtn');
+    answerbtn.textContent = currentQuestionObject.answers[3].text;
+    answerbtn.classList.add('btn', 'answer');
+    document.getElementById('answer-buttons').appendChild(answerbtn);
+    answerbtn.addEventListener('click', function () {
+        selectAnswer(currentQuestionObject.answers[3].correct);
+    })
 }
 
-// If a user runs out of time prior to completing the quiz, they will be shown this message.
+function selectAnswer(isCorrect) {
+    console.log(isCorrect);
+    if (isCorrect == true) {
+        score += 10;
+        alert('Correct! You get 10 points!')
+    } else {
+        sec -= 10;
+        alert('Wrong! you lose 10 seconds!')
+    }
+    currentQuestionIndex++;
+    if (currentQuestionIndex == shuffled.length) {
+        endQuiz();
+    }
+    console.log(currentQuestionIndex);
+    showQuestion(shuffled[currentQuestionIndex]);
+}
+
 function quizTimeout() {
-    alert(`You have run out of time. Here is your score: ${score}`);
+    alert('You have run out of time.');
     endQuiz();
+}
+
+function endQuiz() {
+    alert("The quiz is over");
+    questionContainerEl.classList.add('hide');
+    scores.classList.remove('hide');
+    userInitials = textInput.value();
+    var jsonInitials = JSON.stringify(userInitials);
+    var jsonScore = JSON.stringify(score);
+    localStorage.setItem({user: jsonInitials, score:jsonScore});
+    showLeaderboard();
+}
+
+function showLeaderboard() {
+
 }
 
 // Create the questions and Answers in an array to reference in showQuestion.
@@ -151,58 +192,3 @@ var questionArray = [
         ]
     }
 ]
-
-// function showQuestion(currentQuestionObject) {
-//     // Modify the "question" element in the html to show corresponding question in the questionArray.
-//     questionEl.textContent = currentQuestionObject.questionText;
-//     // Populate each button with corresponding text and add an event listener for when the user selects an answer choice
-//     var answerbtn = document.createElement('answerbtn');
-//     answerbtn.textContent = currentQuestionObject.answers[0].text;
-//     answerbtn.classList.add('btn');
-//     document.getElementById('answer-buttons').appendChild(answerbtn);
-//     answerbtn.addEventListener('click', function () {
-//         selectAnswer(currentQuestionObject.answers[0].correct);
-//     })
-//     var answerbtn = document.createElement('answerbtn');
-//     answerbtn.textContent = currentQuestionObject.answers[1].text;
-//     answerbtn.classList.add('btn');
-//     document.getElementById('answer-buttons').appendChild(answerbtn);
-//     answerbtn.addEventListener('click', function () {
-//         selectAnswer(currentQuestionObject.answers[1].correct);
-//     })
-//     var answerbtn = document.createElement('answerbtn');
-//     answerbtn.textContent = currentQuestionObject.answers[2].text;
-//     answerbtn.classList.add('btn');
-//     document.getElementById('answer-buttons').appendChild(answerbtn);
-//     answerbtn.addEventListener('click', function () {
-//         selectAnswer(currentQuestionObject.answers[2].correct);
-//     })
-//     var answerbtn = document.createElement('answerbtn');
-//     answerbtn.textContent = currentQuestionObject.answers[3].text;
-//     answerbtn.classList.add('btn');
-//     document.getElementById('answer-buttons').appendChild(answerbtn);
-//     answerbtn.addEventListener('click', function () {
-//         selectAnswer(currentQuestionObject.answers[3].correct);
-//     })
-    // var answerButton = document.getElementById('answer-buttons').children[0];
-    // answerButton.textContent = shuffledQuestions.answers[0].text;
-    // answerButton.addEventListener("click", function (event) {
-    //     event.preventDefault();
-    //     selectAnswer(shuffledQuestions.answers[0].correct);
-    // });
-    // var answerButton = document.getElementById('answer-buttons').children[1];
-    // answerButton.textContent = shuffledQuestions.answers[1].text;
-    // answerButton.addEventListener("click", function () {
-    //     selectAnswer(shuffledQuestions.answers[1].correct);
-    // });
-    // var answerButton = document.getElementById('answer-buttons').children[2];
-    // answerButton.textContent = shuffledQuestions.answers[2].text;
-    // answerButton.addEventListener("click", function () {
-    //     selectAnswer(shuffledQuestions.answers[2].correct);
-    // });
-    // var answerButton = document.getElementById('answer-buttons').children[3];
-    // answerButton.textContent = shuffledQuestions.answers[3].text;
-    // answerButton.addEventListener("click", function () {
-    //     selectAnswer(shuffledQuestions.answers[3].correct);
-    // });
-// }
